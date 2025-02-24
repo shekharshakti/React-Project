85197a83e97ef7658bf8186c7fa35effc312aafb
shekharshakti <shekharrajput7050@gmail.com>
2025-02-24 16:13:27 +0530

    editedc

--- React Project/frontend/src/components/auth/Signup.jsx
+++ React Project/frontend/src/components/auth/Signup.jsx
@@ -80,7 +80,7 @@ const Signup = () => {
                             value={input.fullname}
                             name="fullname"
                             onChange={changeEventHandler}
-                            placeholder="patel"
+                            placeholder="Shekhar Shakti"
                         />
                     </div>
                     <div className='my-2'>
@@ -90,7 +90,7 @@ const Signup = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="Shekharrajput7050@gmail.com"
                         />
                     </div>
                     <div className='my-2'>
@@ -100,7 +100,7 @@ const Signup = () => {
                             value={input.phoneNumber}
                             name="phoneNumber"
                             onChange={changeEventHandler}
-                            placeholder="8080808080"
+                            placeholder="7050382908"
                         />
                     </div>
                     <div className='my-2'>
@@ -110,7 +110,7 @@ const Signup = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="shekharrajput7050@gmail.com"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
